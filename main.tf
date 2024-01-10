ae25bff2a753cd462a701eea7b7447b5f6fa675b
Venkat-267 <venkat26072003@gmail.com>
2024-01-10 08:39:17 +0530

    Update commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           =  "ami-0fb653ca2d3203ac1" 
+  ami           =  "ami-03f4878755434977f" 
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
